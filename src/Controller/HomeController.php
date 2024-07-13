e83761025d1d0a87f758537365d8b8077ab51eb8
Michael Dreßler <michdressler@gmail.com>
2024-07-13 17:12:12 +0200

    - update home to show gerichte - add strict_types

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\GerichtRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(GerichtRepository $gerichtRepository): Response
     {
+        $gerichte = $gerichtRepository->findAll();
+
+        $randomGerichte = array_rand($gerichte, 2);
+
+        /*dump(
+            $gerichte[$randomGerichte[0]],
+            $gerichte[$randomGerichte[1]]
+        );*/
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'gericht1' => $gerichte[$randomGerichte[0]],
+            'gericht2' => $gerichte[$randomGerichte[1]],
         ]);
     }
 }
